bd415cc44b0f59207f0f5891c99ae9c6b050713b
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2023-10-17 19:50:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+                name: name,
+                email: email
+            }
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    };
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(res => res.json())
+    .then(data => {document.body.innerHTML = data.id})
+    .catch (err => {document.body.innerHTML = err.message});
+}
